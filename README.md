d0f8810c5afe033f4b9cf73d65c7ca81eb9398f3
Stephan Seifermann <stephan.seifermann@kit.edu>
2019-04-03 13:46:36 +0200

    Initial commit.

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Palladio-Supporting-Workarounds
+The project contains several workarounds for issues that should be fixed in other projects. However, we have to wait for the projects to fix the issue. In the meantime, we apply the workarounds contained in this repository.
+
+## Documentation
+For comprehensive documentation, please visit our [wiki page](https://sdqweb.ipd.kit.edu/wiki/PCM_Development/Workarounds).
+
+## Support
+For support
+* visit our [issue tracking system](https://palladio-simulator.com/jira)
+* contact us via our [mailing list](https://lists.ira.uni-karlsruhe.de/mailman/listinfo/palladio-dev)
+
+For professional support, please fill in our [contact form](http://www.palladio-simulator.com/about_palladio/support/).
---
+++ bundles/org.palladiosimulator.supporting.workarounds.j2ee.activator/src/org/palladiosimulator/product/j2ee/activator/J2EEInitializer.java
@@ -0,0 +1,24 @@
+package org.palladiosimulator.product.j2ee.activator;
+
+import org.eclipse.jst.j2ee.internal.J2EEInit;
+import org.eclipse.ui.IStartup;
+import org.eclipse.wst.common.componentcore.internal.ModulecorePlugin;
+
+@SuppressWarnings("restriction")
+public class J2EEInitializer implements IStartup {
+
+    @Override
+    public void earlyStartup() {
+        /*
+         * J2EE bundles do not initialize the provided EPackages on startup. This is problematic if
+         * code such as OCL assumes that the global package registry is not changed during runtime.
+         * As a workaround, we explicitly initialize the models. Otherwise,
+         * ConcurrentModificationExceptions might occur that are pretty hard to debug. If we do not
+         * use any of these web packages anymore, we can safely delete this whole bundle.
+         */
+        J2EEInit.init(true);
+        J2EEInit.initEMFModels();
+        ModulecorePlugin.getDefault();
+    }
+
+}
